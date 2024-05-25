8c8b923cb16e57be0fa56cec6de3d672b8450491
Yuzhong Zhao <48741629+callsys@users.noreply.github.com>
2024-05-25 19:32:39 +0800

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,4 +4,5 @@ pip install scikit-learn==1.3.2
 pip install SceneGraphParser==0.1.0
 python -m spacy download en
 pip install textblob==0.17.1
+pip install peft==0.8.2
 pip install DCNv4==1.0.0.post2
