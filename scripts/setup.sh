7e55f7572db564902e7a83c1876c35e64bc1e860
Yuzhong Zhao <48741629+callsys@users.noreply.github.com>
2024-05-25 18:18:15 +0800

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,1 +1,8 @@
-
+conda create -n dynrefer python=3.8 -y && conda activate dynrefer
+pip install --upgrade pip
+pip install salesforce-lavis
+pip install scikit-learn
+pip install SceneGraphParser
+python -m spacy download en
+pip install textblob
+pip install DCNv4
